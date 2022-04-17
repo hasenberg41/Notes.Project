31c7239dac99bf7763baf1caf8ab460e81b0cc93
Никита Кондрашов <hasenberg41@mail.ru>
2022-04-17 12:21:18 +0300

    Added EntityFrameworkCore configure module

---
+++ Notes.Persistance/EntityTypeConfiguration/NoteConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Notes.Domain;
+
+namespace Notes.Persistance.EntityTypeConfiguration
+{
+    public class NoteConfiguration : IEntityTypeConfiguration<Note>
+    {
+        public void Configure(EntityTypeBuilder<Note> builder)
+        {
+            builder.HasKey(n => n.Id);
+            builder.HasIndex(n => n.Id).IsUnique();
+            builder.Property(n => n.Title).HasMaxLength(250);
+        }
+    }
+}
