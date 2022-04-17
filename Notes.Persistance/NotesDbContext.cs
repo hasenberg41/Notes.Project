18a3c9bdda1297abf449054fef5eb2e7b2311640
Никита Кондрашов <hasenberg41@mail.ru>
2022-04-17 12:43:33 +0300

    Added class DbContext and using INotesDbContxt and DbContext

---
+++ Notes.Persistance/NotesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+using Notes.Persistance.EntityTypeConfiguration;
+using Notes.Domain;
+
+namespace Notes.Persistance
+{
+    public class NotesDbContext : DbContext, INotesDbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+        public NotesDbContext(DbContextOptions options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            model.ApplyConfiguration(new NoteConfiguration());
+            base.OnModelCreating(model);
+        }
+    }
+}
